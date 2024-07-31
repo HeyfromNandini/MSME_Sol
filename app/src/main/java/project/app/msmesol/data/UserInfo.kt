55476cfa8fd627876afe5f78de0b19354628ec6a
thekaailashsharma <kailashps.1011@gmail.com>
2024-08-01 00:49:00 +0530

    Minor Changes

---
+++ app/src/main/java/project/app/msmesol/data/UserInfo.kt
@@ -0,0 +1,20 @@
+package project.app.msmesol.data
+
+data class UserInfo(
+    val name: String,
+    val phoneNumber: String,
+    val businessAddress: String,
+    val websiteUrl: String,
+    val businessName: String,
+    val businessType: String,
+    val registrationNumber: String,
+    val yearOfEstablishment: String,
+    val companySize: String,
+    val annualRevenue: String,
+    val gstinNumber: String,
+    val panNumber: String,
+    val udyamNumber: String,
+    val selectedIdentityProof: String,
+    val fileUri: String?
+
+)
